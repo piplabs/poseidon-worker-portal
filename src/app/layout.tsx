4bccec691b089e980fc4b2113a8aae4403d6e7fe
Aryan Parekh <aryanparekh@Aryans-MacBook-Pro.local>
2025-10-14 16:49:11 -0700

    stake

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from '@/app/providers';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Poseidon Bridge",
-  description: "Poseidon Bridge",
+  title: "Poseidon Worker Portal",
+  description: "Poseidon Worker Portal",
   icons: {
     icon: "https://psdn.ai/icon.png?07720b992e581016",
     shortcut: "https://psdn.ai/icon.png?07720b992e581016",
@@ -31,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased font-sans`}
       >
         <Providers>
           {children}
